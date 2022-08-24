a10091eb699c04c6883071b1ea5bca36ee0fe9c3
mel291295 <102040009+mel291295@users.noreply.github.com>
2022-08-24 11:40:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 # ! python3
+#import all the necessary libraries
 import streamlit as st 
+import os
+from gtts import gTTS
+from googletrans import Translator
 
 st.title("Once upon a time")
 st.header("Analyze Gimm`s fairy tales")
 
+
+
+#add sidebar with some basic information about the app
